1c02d1f018f028cb281b06b195c9b928fb8c6911
unknown <FOO@OCHALT-142733.unog.un.org>
2016-07-12 18:47:24 +0200

    Changed to query for whole emergency

--- flowCompare.js
+++ flowCompare.js
@@ -4,48 +4,125 @@
  * Loop over each result and then construct the simple view
  * 
  */
-
 "use strict";
 var dto = require("./dto.js");
 var fs = require("fs");
 var config = require("./config.js");
 
-var outputFilepath = config.localfileOutputPath + "spotCheckData.json"; 
+var urlFlow = config.url_hpcapi + "flow/legacy/";
+var urlcontri = config.url_ftsapi + "contribution/emergency/";
 
-var url = config.url_hpcapi + "flow/legacy/";
+// let the user specify the emergency id
+var emergencyId = process.argv[2];
+if(isNaN(emergencyId)){
+  console.error("Invalid emergencyId");
+  throw ("Invalid emergencyId");
+}
+var outputFilepath = config.localfileOutputPath + "CheckData_" + emergencyId + ".json"; 
 
-var legacyIds = []; 
-legacyIds.push(223962);// Donor to recipient in emergency
-legacyIds.push(212912); // donor to unknown recipient in emergency
-legacyIds.push(213596); // donor to project in appeal
-
+// global arrays
+var aryContributions = [];
 var aryResults = [];
-var aryP = [];
 
-// for each id get the flow
-legacyIds.forEach(element=> {
-	let oflow = new dto.sourceAPI(url + element);
+console.log("start");
 
-	let p1 = oflow.getAPIData()
-	.then((jdata)=>{
-		oflow.jdata = jdata;
-		aryResults.push(dto.flowSimple(jdata.data.flow));
-
-	});
-
-	aryP.push(p1);
-
-});
-
-// when all the promises are done then output results
-Promise.all(aryP)
-.then(values=>{
-	//console.log(aryResults);
-
-	fs.writeFile(outputFilepath,JSON.stringify(aryResults), (err)=>{
-    if(err){ console.error(err);}
+// get the contribution API output first
+var oContribution = new dto.sourceAPI(urlcontri + emergencyId + ".json");
+var p0 = oContribution.getAPIData()
+.then(jdata=>{
+  po.jdata = jdata;
+  jdata.forEach(element=>{ // just get the contribution id's into an array
+    aryContributions.push(element.id);
   });
-});
 
-// output results
+  console.log("length = " + aryContributions.length);
+  
+  // now throttle process the array of contribution id's
+  throttle(aryContributions,0,50);
+
+})
+.catch(function(err){
+  console.error(err);
+});  
+
+/** @global {int} current - to keep track of the current position for the batch processing */
+var current;
+/**
+ * to batch process an array of contribution id's, recursive call
+ * @param {array} ary - of contribution id's
+ * @param {int} start - the position to use to start and continue the array processing
+ * @param {int} batchsize - the batch size
+ */
+function throttle(ary, start, batchsize){
+  
+  current = start;
+
+  console.log("throttle "+ current);
+
+  // logic to work out how many to left to process and adjust the batchsize accordingly
+  var tlength = ary.length;
+  var remainder = tlength % batchsize;
+  var limit = tlength - remainder;
+  if(current === limit){
+    batchsize = remainder;
+  }
+  else if(tlength < batchsize){
+    batchsize = tlength;
+  }
+
+  var lary = [];
+  // get the current batch to process
+  for(let i=0; i < batchsize; i++){
+    lary[i] = ary[current];
+    current++;
+  }
+
+  // process the current batch
+  var P = processFlows(lary);
+  P.then(()=>{
+    if(current === tlength){ // if last batch and no more to process
+      console.log("output " + aryResults.length);
+      fs.writeFile(outputFilepath,JSON.stringify(aryResults), (err)=>{
+          if(err){ console.error(err);}
+      });
+      return true;
+    }
+    else{ // recursive call to next batch
+      throttle(ary,current,batchsize);
+    }
+    
+  });
+
+}
+
+
+
+
+
+/**
+ * to process an array of contribution id's
+ * @param {array} of contribution id's
+ */
+function processFlows(aryFlows){
+  var aryP = [];
+
+  aryFlows.forEach(element=> {
+    let oflow = new dto.sourceAPI(urlFlow + element);
+    //console.log(element);
+
+    let p1 = oflow.getAPIData()
+    .then((jdata)=>{
+      oflow.jdata = jdata;
+      aryResults.push(dto.flowSimple(jdata.data.flow));
+      
+    })
+    .catch(function(err){
+      console.error(err);
+    });  
+    aryP.push(p1);
+  });
+
+  // when all the promises are done then output results
+  return Promise.all(aryP);
+}
 
